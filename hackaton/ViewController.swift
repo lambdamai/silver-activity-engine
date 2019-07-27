88aa5b60e2bb678b1097db0159640b117b5d0c8a
godemodegame <godemodegame@icloud.com>
2019-07-27 13:48:27 +0300

    Initial Commit

---
+++ hackaton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hackaton
+//
+//  Created by godemodegame on 27/07/2019.
+//  Copyright © 2019 godemodegame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
